f24ba9d8a5da554d4fef2657888ce328e92c3cc9
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-10-12 21:32:21 +0100

    feat: ui implementation for sign in page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen w-screen overflow-hidden bg-pink-100">
+      <div className="h-2/3 w-2/3 md:w-1/4 border-2 border-pink-400 bg-white rounded-2xl flex flex-col items-center justify-center">
+        <img
+          src="/images/login.png"
+          width="100"
+          height="100"
+          alt="sign in girl"
+        />
+        <p className="font-bold text-xl py-5">Would you Rather?</p>
+        <form className="flex flex-col">
+          <label className="text-lg font-medium pb-3" htmlFor="users">
+            Select your user
+          </label>
+          <select
+            className="appearance-none border-2 border-pink-200 rounded-md px-5"
+            name="users"
+          >
+            <option className="appearance-none" value="1">
+              1
+            </option>
+            <option className="appearance-none" value="2">
+              2
+            </option>
+            <option className="appearance-none" value="3">
+              3
+            </option>
+          </select>
+          <button className="bg-pink-600 text-white font-bold rounded-lg my-5 py-2" type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
