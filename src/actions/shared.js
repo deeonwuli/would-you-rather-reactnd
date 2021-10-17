22523b4665d967e58de5922940da67a41f0708f7
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-10-17 17:57:53 +0100

    feat: actions folder update
    updated the project with what i think are suitable actions and moved _DATA.JS file into a separate folder

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from "../utils/api";
+import { receiveUsers } from "./users";
+import { receiveQuestions } from "./questions";
+import { setAuthedUser } from "./authedUser";
+
+const AUTHED_ID = "sarahedo"
+
+export function handleInitialData () {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, questions }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveQuestions(questions))
+        dispatch(setAuthedUser(AUTHED_ID))
+      })
+  }
+}
