193f68a9ae42a40234bf9dc2780729555e49b05d
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-11-05 13:48:24 +0100

    refactor: make 404 page more responsive

--- src/components/404page.js
+++ src/components/404page.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export default function Page404 () {
   return (
-    <div className="w-screen h-screen bg-pink-100 text-4xl flex flex-col justify-center items-center font-bold space-y-4">
-      <p>Uh oh. It looks like this page you're trying to visit does not exist.</p>
+    <div className="w-screen h-screen bg-pink-100 text-4xl flex flex-col justify-center items-center font-bold space-y-4 px-4">
+      <p className="text-center">Uh oh. It looks like this page you're trying to visit does not exist.</p>
       <Link to="/" className="bg-pink-600 text-white text-xl px-5 py-2 rounded-xl">
         Go back home
       </Link>
