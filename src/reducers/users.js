0e1077b61718b30c47c01291f5c22c676a075282
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-11-05 21:51:15 +0100

    fix: leaderboard is not updated when a new question is created

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,5 @@
 import { RECEIVE_USERS } from "../actions/users";
+import { ADD_QUESTION, ADD_ANSWER } from "../actions/questions";
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -6,6 +7,28 @@ export default function users(state = {}, action) {
       return {
         ...state,
         ...action.users,
+      };
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.author]: {
+          ...state[action.question.author],
+          questions: state[action.question.author].questions.concat([
+            action.question.id,
+          ]),
+        },
+      };
+    case ADD_ANSWER:
+      const { id, answer, authedUser } = action.answerInfo;
+      return {
+        ...state,
+        [authedUser]: {
+          ...state[authedUser],
+          answers: {
+            ...state[authedUser].answers,
+            [id]: answer,
+          },
+        },
       };
     default:
       return state;
